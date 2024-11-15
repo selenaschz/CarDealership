57b3b8350a42b83ccf032f8c301de09aa24e53cd
Selena Sánchez <sele71098@gmail.com>
2024-11-15 23:44:23 +0100

    create project and start designing LoginActivity.xml

---
+++ app/src/main/java/com/uax/cardealership/adapters/BrandsAdapter.kt
@@ -0,0 +1,4 @@
+package com.uax.cardealership.adapters
+
+class BrandsAdapter {
+}
---
+++ app/src/main/java/com/uax/cardealership/ui/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.uax.cardealership.ui
+
+import android.os.Binder
+import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.uax.cardealership.R
+import com.uax.cardealership.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
